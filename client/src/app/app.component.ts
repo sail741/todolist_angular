7e25598e3b3e5b0cde3676767e1ce03f4e25dd5c
sail741 <leo.mullot@gmail.com>
2020-03-26 23:20:31 +0100

    ask user to update the app when a new version is available

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import {UpdateService} from './update.service';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UpdateService]
 })
 export class AppComponent {
+  constructor(private update: UpdateService) {
+  }
   title = 'client';
 }
---
+++ client/src/app/update.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+import { MatSnackBar } from '@angular/material';
+import { SwUpdate } from '@angular/service-worker';
+
+@Injectable()
+export class UpdateService {
+  constructor(private swUpdate: SwUpdate, private snackbar: MatSnackBar) {
+    this.swUpdate.available.subscribe(evt => {
+      const snack = this.snackbar.open('Update Available', 'Reload');
+
+      snack.onAction().subscribe(() => {
+          window.location.reload();
+        });
+
+      setTimeout(() => {
+        snack.dismiss();
+      }, 6000);
+    });
+  }
+}
